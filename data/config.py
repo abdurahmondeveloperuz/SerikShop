df8524b1b7e9960c9673e00d20fa97740232c34c
abdurahmondeveloperuz <154701401+abdurahmondeveloperuz@users.noreply.github.com>
2023-12-31 00:27:12 +0500

    Update config.py

--- data/config.py
+++ data/config.py
@@ -1,7 +1,7 @@
 from environs import Env
 env = Env()
 env.read_env()
-BOT_TOKEN = env.str("BOT_TOKEN")  
-ADMINS = env.list("ADMINS") 
-IP = env.str("ip")  
-fivesimnettoken = env.str('SIM_NET_API_TOKEN')
+BOT_TOKEN = "6381495585:AAFgR5kCwPeEcoiCO4rZDr-t7RgSLhd9jm4"  
+ADMINS = ["6379220588","1002468249","5165260150"]
+IP = "localhost"
+fivesimnettoken = 8f2zKvQjFrtzU86-e7gHV3HG-YBzT5Q65-7qC95pnL-6CnSC2DdvRQWH88
